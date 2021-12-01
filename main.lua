4695a2749adb06145d6fea45810bb972817fe475
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2021-12-01 20:24:37 +0100

    descriptive names

--- main.lua
+++ main.lua
@@ -1,19 +1,19 @@
-
+local star_size,star_empty,star_filled
 function love.load()
-star1=love.graphics.newImage("star1.png")
-star2=love.graphics.newImage("star2.png")
+star_size=64
+star_empty=love.graphics.newImage("star_empty.png")
+star_filled=love.graphics.newImage("star_filled.png")
 end
 
 function love.draw()
-local size=64
-local vote=love.mouse.getX()/size
+local vote=love.mouse.getX()/star_size
 local which_star
 for i=0,4 do
     if i<vote then
-        which_star=star2
+        which_star=star_filled
     else
-        which_star=star1
+        which_star=star_empty
     end
-    love.graphics.draw(which_star,i*size,0)
+    love.graphics.draw(which_star,i*star_size,0)
 end
 end
