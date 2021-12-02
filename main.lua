d27e461f267268a664065b6f4802dcc382eb706b
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2021-12-02 17:42:56 +0100

    default max=5, optionally settable in widget (e.g. max=7)

--- main.lua
+++ main.lua
@@ -13,8 +13,12 @@ return point[1]>=rectangle[1] and
 end
 
 function star_vote_draw(star_vote)
+local max=5
+if star_vote.max~=nil then
+    max=star_vote.max
+end
 local ox,oy=star_vote[1],star_vote[2]
-local rectangle={ox,oy,star_size*5, star_size}
+local rectangle={ox,oy,star_size*max, star_size}
 local point={love.mouse.getX(),love.mouse.getY()}
 local vote
 if point_in_rectangle_check(point,rectangle) then
@@ -26,7 +30,7 @@ else
     vote=star_vote.vote
 end
 local which_star
-for i=0,4 do
+for i=0,(max-1) do
     if i<vote then
         which_star=star_filled
     else
@@ -37,7 +41,7 @@ end
 end
 
 local star_vote1={50,0,vote=1}
-local star_vote2={0,star_size,vote=2}
+local star_vote2={0,star_size,vote=2,max=7}
 function love.draw()
 star_vote_draw(star_vote1)
 star_vote_draw(star_vote2)
